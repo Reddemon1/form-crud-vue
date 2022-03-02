a8fdfb149c6bef1a9c5823b8de88201c634a168a
Reddemon1 <rionaldo0567@gmail.com>
2022-03-02 15:58:46 +0700

    Initial commit

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SiswaRequest;
+use App\Models\Siswa;
+use App\Http\Requests\StoreSiswaRequest;
+use App\Http\Requests\UpdateSiswaRequest;
+// use App\Http\Requests\SiswaRequest;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Siswa::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSiswaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SiswaRequest $request)
+    {
+        $siswa = Siswa::create($request->validated());
+
+        return Siswa::all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Siswa $siswa)
+    {
+        return $siswa;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Siswa $siswa)
+    {
+        return $siswa;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSiswaRequest  $request
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(SiswaRequest $request, Siswa $siswa)
+    {
+        $siswa = $siswa->update($request->validated());
+
+        return Siswa::all();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Siswa $siswa)
+    {
+        $siswa->delete();
+
+        return Siswa::all();
+    }
+}
